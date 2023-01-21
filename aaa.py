ef6d7d4ef46add34b2ea29790f5cffe359fa5b88
devlooppear <115846304+devlooppear@users.noreply.github.com>
2023-01-21 17:24:39 -0300

    need_fix

---
+++ aaa.py
@@ -0,0 +1,4 @@
+#Aqui é o que eu tô tentando fazer
+num = "667.5"
+print(len(num))
+num.replace("mi",f"{'0'* 9 - len(num)}")
--- main.py
+++ main.py
@@ -19,39 +19,130 @@ conn.autocommit = True
 
 cursor = conn.cursor()
 
+sql = 'DROP TABLE IF EXISTS imobiliaria'
+cursor.execute(sql)
+
+sql = f'''CREATE TABLE imobiliaria(
+id SERIAL NOT NULL,
+nomes_cam VARCHAR (10) NOT NULL,
+liq_dia INT NOT NULL,
+ult_rend FLOAT NOT NULL,
+div_yield FLOAT NOT NULL,
+pat_liq INT NOT NULL,
+vlr_pat FLOAT NOT NULL,
+ren_mes FLOAT NOT NULL,
+p_vp FLOAT NOT NULL,
+PRIMARY KEY (id)
+);'''
+cursor.execute(sql)
+
 caminhos = ['mxrf11', 'vghf11', 'bcff11', 'vslh11', 'game11', 'mchf11', 'hglg11', 'xpml11', 'knri11', 'hgre11', 'hgcr11', 'rztr11', 'hgru11', 'vgia11', 'vghf11', 'xpml11', 'visc11', 'mall11', 'lvbi11']
 
+for caminho in caminhos:
 
-dados = []
-for c in caminhos:
-    navegador.get(f"https://www.fundsexplorer.com.br/funds/{c}")
+    navegador.get(f"https://www.fundsexplorer.com.br/funds/{caminho}")
 
     titulos_liq_dia =[]
 
     #Ali no final, se você botar '//div[@id="main-indicators-carousel"]//div[@class="flickity-viewport"]//div[@class="flickity-slider"]//div[@class="carousel-cell is-selected"]//span[2]' aparecem só os números, ok
-    valores_liq_dia = navegador.find_elements(By.XPATH, '//div[@id="main-indicators-carousel"]//div[@class="flickity-viewport"]//div[@class="flickity-slider"]//div[@class="carousel-cell is-selected"]//span')
+    valores_liq_dia = navegador.find_elements(By.XPATH, '//div[@id="main-indicators-carousel"]//div[@class="flickity-viewport"]//div[@class="flickity-slider"]//div[1][@class="carousel-cell is-selected"]//span[2]')
 
-    for v in valores_liq_dia: 
-        v = v.text
-        if v != " ":
-            titulos_liq_dia.append(v)
-    
-    for a in titulos_liq_dia:
-        a = str(a)
-        a = a.replace(",",".").replace("negociações","")
-        print(a)
-    
-'''sql = 'DROP TABLE IF EXISTS imobiliaria'
-cursor.execute(sql)
+    for vlr_liq in valores_liq_dia: 
+        vlr_liq = vlr_liq.text
+        if vlr_liq != " ":
+            titulos_liq_dia.append(vlr_liq)
+        print(vlr_liq)
 
-sql2 = 'CREATE TABLE imobiliaria(
-id SERIAL NOT NULL,
-liq_dia INT NOT NULL,
-ult_rend INT NOT NULL,
-div_yield INT NOT NULL,
-vlr_pat INT NOT NULL,
-ren_mes INT NOT NULL,
-p_vp INT NOT NULL,
-PRIMARY KEY (id)
-);'
-cursor.execute(sql2)'''
+    titulos_ult_rend =[]
+
+    valores_ult_rend = navegador.find_elements(By.XPATH, '//div[@id="main-indicators-carousel"]//div[@class="flickity-viewport"]//div[@class="flickity-slider"]//div[2][@class="carousel-cell is-selected"]//span[2]')
+
+    for vlr_ult in valores_ult_rend: 
+        vlr_ult = vlr_ult.text
+        if vlr_ult != " ":
+            titulos_ult_rend.append(vlr_ult)
+    
+    titulos_div_yield =[]
+
+    valores_div_yield = navegador.find_elements(By.XPATH, '//div[@id="main-indicators-carousel"]//div[@class="flickity-viewport"]//div[@class="flickity-slider"]//div[3][@class="carousel-cell is-selected"]//span[2]')
+
+    for vlr_div in valores_div_yield: 
+        vlr_div = vlr_div.text
+        if vlr_div != " ":
+            titulos_div_yield.append(vlr_div)
+
+    titulos_pat_liq =[]
+
+    valores_pat_liq = navegador.find_elements(By.XPATH, '//div[@id="main-indicators-carousel"]//div[@class="flickity-viewport"]//div[@class="flickity-slider"]//div[4][@class="carousel-cell is-selected"]//span[2]')
+
+    for vlr_pat_liq in valores_pat_liq: 
+        vlr_pat_liq = vlr_pat_liq.text
+        if vlr_pat_liq != " ":
+            titulos_pat_liq.append(vlr_pat_liq)
+
+    titulos_pat =[]
+
+    valores_pat = navegador.find_elements(By.XPATH, '//div[@id="main-indicators-carousel"]//div[@class="flickity-viewport"]//div[@class="flickity-slider"]//div[5][@class="carousel-cell is-selected"]//span[2]')
+
+    for vlr_pat in valores_pat: 
+        vlr_pat = vlr_pat.text
+        if vlr_pat != " ":
+            titulos_pat.append(vlr_pat)
+    
+    titulos_ren_mes =[]
+
+    valores_ren_mes = navegador.find_elements(By.XPATH, '//div[@id="main-indicators-carousel"]//div[@class="flickity-viewport"]//div[@class="flickity-slider"]//div[6][@class="carousel-cell is-selected"]//span[2]')
+
+    for vlr_ren in valores_ren_mes: 
+        vlr_ren = vlr_ren.text
+        if vlr_ren != " ":
+            titulos_ren_mes.append(vlr_ren)
+    
+    titulos_p_vp = []
+
+    valores_p_vp = navegador.find_elements(By.XPATH, '//div[@id="main-indicators-carousel"]//div[@class="flickity-viewport"]//div[@class="flickity-slider"]//div[7][@class="carousel-cell is-selected"]//span[2]')
+
+    for vlr_p in valores_p_vp: 
+        vlr_p = vlr_p.text
+        if vlr_p != " ":
+            titulos_p_vp.append(vlr_p)
+    
+    for nome,a,b,c,d,e,f,d in list(zip(caminhos,titulos_liq_dia,titulos_ult_rend,titulos_div_yield,titulos_pat_liq,titulos_pat,titulos_ren_mes,titulos_p_vp)):
+
+        for a in titulos_liq_dia:
+            a = str(a)
+            a = a.replace(".","")
+            print(a)
+        
+        for b in titulos_ult_rend:
+            b = str(b)
+            b = b.replace(",",".").replace("R","").replace("$","").replace(" ","")
+            print(b)
+
+        for c in titulos_div_yield:
+            c = str(c)
+            c = c.replace(",",".").replace("%","")
+            print(c)
+
+            
+        for d in titulos_pat_liq:
+            d = str(d)
+            d = d.replace(",",".").replace("R","").replace("$","").replace(" ","").replace("mi","").replace(".","").replace("bi","")
+            print(d)
+
+        for e in titulos_pat:
+            e = str(e)
+            e = e.replace(",",".").replace("R","").replace("$","").replace(" ","")
+            print(e)
+
+        for f in titulos_ren_mes:
+            f = str(f)
+            f = f.replace(",",".").replace("%","")
+            print(f)
+        for g in titulos_p_vp:
+            g = str(g)
+            g = g.replace(",",".")
+            print(g)
+
+        sql = f'''INSERT INTO imobiliaria (nomes_cam,liq_dia,ult_rend,div_yield,pat_liq,vlr_pat,ren_mes,p_vp) VALUES ('{nome}','{int(a)}','{float(b)}','{float(c)}','{int(d)}','{float(e)}','{float(f)}','{float(g)}')'''
+        cursor.execute(sql)
